19e78ef40a4d21a2e447c58e1b58dce745940d14
Jamie-Poeffel <Jamie_poeffel@sluz.ch>
2024-12-13 15:20:19 +0100

    go-3-ex-3 zusatzaufgabe

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -12,14 +12,28 @@ func main() {
 
 	for i := Lower; i <= Upper; i++ {
 		fmt.Print(i, ". ")
-		if i%5 == 0 && i % 3 == 0 {
-			fmt.Println("FizzBuzz")
-		}else if i % 3 == 0 {
-			fmt.Println("Fizz")
-		}else if i % 5 == 0{
-			fmt.Println("Buzz")
-		}else {
-			fmt.Println(i)
+		switch {
+			case i%5 == 0 && i % 3 == 0: {
+				fmt.Println("FizzBuzz")
+			}
+			case i % 3 == 0: {
+				fmt.Println("Fizz")
+			}
+			case i % 5 == 0: {
+				fmt.Println("Buzz")
+			}
+			default: {
+				fmt.Println(i)
+			}
 		}
+		// if i%5 == 0 && i % 3 == 0 {
+		// 	fmt.Println("FizzBuzz")
+		// }else if i % 3 == 0 {
+		// 	fmt.Println("Fizz")
+		// }else if i % 5 == 0{
+		// 	fmt.Println("Buzz")
+		// }else {
+		// 	fmt.Println(i)
+		// }
 	}
 }
